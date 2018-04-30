f3996a54c0fbc4563066d0b624ba2aeeb7286b7a
Lajt <matti815@gmail.com>
2018-04-30 17:25:39 +0200

    overall improvments

---
+++ client/src/DrawingList.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react'
+import {subscribeToDrawings} from './api'
+
+class DrawingList extends Component{
+  constructor(props){
+    super(props)
+
+    subscribeToDrawings((drawing) => {
+      this.setState(prevState => ({
+        drawings: prevState.drawings.concat([drawing])
+      }))
+    })
+  }
+
+  state = {
+    drawings: []
+  }
+
+  render(){
+    const drawings = this.state.drawings.map(drawing => (
+      <li className="DrawingList-item"
+          key={drawing.id}>
+        {drawing.name}
+      </li>
+    ))
+    return(
+      <div>
+        <ul className="DrawingList">
+          {drawings}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default DrawingList
